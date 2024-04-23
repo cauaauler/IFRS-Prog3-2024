c70556fdf5027bb82cf8442e33226ee0f1efa11c
Cauã Auler <cauaauler@gmail.com>
2024-04-23 15:46:11 -0400

    .

--- aula05-exer/script.js
+++ aula05-exer/script.js
@@ -1,8 +1,17 @@
 let aumentar = document.querySelector(".aumentar");
 let subtrair = document.querySelector(".subtrair");
 let resetar = document.querySelector(".resetar");
-let contador = document.querySelector(".contador");
+let contador = document.getElementsByClassName("contador")[1];
+
+var numeroContador = 0;
 
 aumentar.addEventListener('click', function(e){
-    contador.innerHTML = parseInt(contador.innerHTML) ++;
+    contador.innerHTML = ++numeroContador;
+})
+subtrair.addEventListener('click', function(e){
+    contador.innerHTML = --numeroContador;
+})
+resetar.addEventListener('click', function(e){
+    contador.innerHTML = 0;
+    numeroContador = 0;
 })
