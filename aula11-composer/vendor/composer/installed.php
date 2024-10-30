900c667262e800b76a5d214232682835fd949328
Cauã Auler <cauaauler@gmail.com>
2024-10-30 11:18:15 -0300

    começando com composer

---
+++ aula11-composer/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'aluno/aula11-composer',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0b8bd1da92d49dd0bacd407d0df1424d0a4b1b9e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'aluno/aula11-composer' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0b8bd1da92d49dd0bacd407d0df1424d0a4b1b9e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
